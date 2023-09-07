78d61f6cc227ab03c0d6cf2b83d97b571b0298fd
CHARBIT Jonathan <jonathanchpro@gmail.com>
2023-09-07 23:22:50 +0200

    admin part 5 42:10 sauvegarde ok

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -5,39 +5,50 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
+use Illuminate\Support\Facades\Session;
 
 class CategoryController extends Controller
 {
+
     public function index() {
+        $categories = Category::latest()->pagination(10);
 
+        return view ('admin.category.list');
     }
 
-    public function create() {
-        return view ('admin.category.create');
+    public function create()
+    {
+        return view('admin.category.create');
     }
 
-    public function store(Request $request) {
-        $validator = Validator::make($request->all(),[
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'slug' => 'required|unique:categories',
         ]);
 
         if ($validator->passes()) {
+            $category = new Category();
+            $category->name = $request->input('name');
+            $category->slug = $request->input('slug');
+            $category->status = $request->input('status');
+            $category->save();
 
+            Session::flash('success', 'La catégorie a été ajoutée');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'La catégorie a été ajoutée',
+            ]);
         } else {
             return response()->json([
                 'status' => false,
-                'errors' => $validator->errors()
+                'errors' => $validator->errors(),
             ]);
         }
     }
 
-    public function edit() {
-
-    }
-    public function destroy() {
-
-    }
-
-
+    // ... autres méthodes du contrôleur ...
 }
